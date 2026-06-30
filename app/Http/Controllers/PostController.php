9e455e1408a8d48cb92ed69e1976c155e678460d
sara charafi <elkbircharafi8@gmail.com>
2026-06-30 17:14:22 +0100

    chore: initialize Laravel project

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StorePostRequest;
+
+class PostController extends Controller
+{
+    public function store(StorePostRequest $request)
+    {
+        $validated = $request->validated();
+
+        return $validated;
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,5 @@
 
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
 <?php
 
 use App\Http\Controllers\ProfileController;
