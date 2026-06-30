fabfea6eb80a11e59f2a9437e6777cf78598fd95
sara charafi <elkbircharafi8@gmail.com>
2026-06-30 17:23:05 +0100

    feat: install Laravel Breeze authentication scaffolding

--- app/Http/Middleware/PreventRequestsDuringMaintenance.php
+++ app/Http/Middleware/PreventRequestsDuringMaintenance.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Illuminate\Foundation\Http\Middleware\PreventRequestsDuringMaintenance as Middleware;
 
+
 class PreventRequestsDuringMaintenance extends Middleware
 {
     /**
